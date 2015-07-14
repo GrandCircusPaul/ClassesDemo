837d6ec0ccd167d3166c21fbd4aa58189cb722ed
GrandCircusPaul <paul@grandcircus.com>
2015-07-14 09:31:48 -0400

    moved class to separate file
    The Box class is now in its own file, Box.cs.  It uses the same
    namespace as before, which is still the same as the Program class that
    contains the Main() function.

---
+++ ClassesDemo/Box.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesDemo
+{
+    class Box
+    {
+        private static int boxCount;
+        private int length;
+        private int width;
+        private int height;
+        private bool full;
+
+        public bool Full
+        {
+            get
+            {
+                return full;
+            }
+            set
+            {
+                full = value;
+            }
+        }
+
+        public Box() // The DEFAULT CONSTRUCTOR
+        {
+            boxCount++;
+            this.length = 1;
+            this.width = 2;
+            this.height = 3;
+            this.full = false;
+
+            Console.WriteLine("Created Box Number " + boxCount);
+        }
+
+        public Box(int _length, int _width, int _height, bool _full) // An OVERLOADED CONSTRUCTOR
+        {
+            boxCount++;
+            this.length = _length;
+            this.width = _width;
+            this.height = _height;
+            this.full = _full;
+
+            Console.WriteLine("Created Box Number " + boxCount);
+        }
+        public Box(int _length, int _width)//, int _height) // An OVERLOADED CONSTRUCTOR
+        {
+            boxCount++;
+            this.length = _length;
+            this.width = _width;
+            //            this.height = _height;
+            this.full = false;
+
+            Console.WriteLine("Created Box Number " + boxCount);
+        }
+        public Box(int _length, int _width, int _height) // An OVERLOADED CONSTRUCTOR
+        {
+            boxCount++;
+            this.length = _length;
+            this.width = _width;
+            this.height = _height;
+            this.full = false;
+
+            Console.WriteLine("Created Box Number " + boxCount);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("( l , w , h ) == (\t" + this.length + ",\t" + width + ",\t" + height + ")");
+            Console.WriteLine("Volume: " + computeVolume());
+            Console.WriteLine("Full: " + full);
+        }
+
+        private int computeVolume()
+        {
+            return length * width * height;  // Does not use "this" keyword
+        }
+
+
+    }
+}
